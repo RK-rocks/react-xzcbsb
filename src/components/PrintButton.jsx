7d6f46a096c3a3c178bef070b085b08b6445aa86
webwerks <raj.kamadiya@neosofttech.com>
2020-07-02 14:56:25 +0530

    add page in pdf

--- src/components/PrintButton.jsx
+++ src/components/PrintButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
+import Canvas2Image from 'canvas2image'
 
 const pxToMm = (px) => {
   return Math.floor(px / document.getElementById('myMm').offsetHeight);
@@ -27,9 +28,10 @@ const PrintButton = ({ id, label }) => (<div className="tc mb4 mt2">
     className="pa2 ba bw1 b--black bg-yellow black-90 br2 dib pointer dim shadow-1"
     onClick={() => {
       const input = document.getElementById(id);
+      const inputHeightPx = mmToPx(input.offsetHeight)
       const inputHeightMm = pxToMm(input.offsetHeight);
       const a4WidthMm = 210;
-      const a4HeightMm = 410;
+      const a4HeightMm = 295;
       const a4HeightPx = mmToPx(a4HeightMm);
       const numPages = inputHeightMm <= a4HeightMm ? 1 : Math.floor(inputHeightMm / a4HeightMm) + 1;
       console.log({
@@ -38,25 +40,59 @@ const PrintButton = ({ id, label }) => (<div className="tc mb4 mt2">
       });
 
 
-      html2canvas(input)
-        .then((canvas) => {
-          const imgData = canvas.toDataURL('image/png');
-          console.log(a4HeightMm)
-          let pdf
-          // Document of a4WidthMm wide and inputHeightMm high
-          if (inputHeightMm > a4HeightMm) {
-            // elongated a4 (system print dialog will handle page breaks)
-            console.log("in if")
-            pdf = new jsPDF('p', 'mm', [inputHeightMm + 99, a4WidthMm]);
-          } else {
-            console.log("in else")
-            // standard a4
-            pdf = new jsPDF('p', 'mm', [inputHeightMm + 89, a4WidthMm]);
-          }
 
-          pdf.addImage(imgData, 'PNG', 0, 0);
-          pdf.save(`${id}.pdf`);
-        });
+      html2canvas(input, {
+        allowTaint: false,
+        removeContainer: true,
+        backgroundColor: '#ffffff',
+        scale: window.devicePixelRatio,
+        useCORS: false
+      }).then(canvas => {
+        const contentDataURL = canvas.toDataURL('image/png', 1.0)
+        const imgWidth = 210;
+        const pageHeight = 295;
+        const imgHeight = canvas.height * imgWidth / canvas.width;
+        console.log('imgHeight', imgHeight)
+        console.log('imgWidth', imgWidth)
+        let heightLeft = imgHeight;
+
+        let pdf = new jsPDF('1', 'mm', 'a4'); // A4 size page of PDF
+
+
+        let position = 5;
+
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+        heightLeft -= pageHeight;
+
+        while (heightLeft >= 0) {
+          position = heightLeft - imgHeight;
+          pdf.addPage();
+          pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+          heightLeft -= pageHeight;
+        }
+        pdf.save(`${id}.pdf`); // Generated PDF
+      });
+
+
+      // html2canvas(input)
+      //   .then((canvas) => {
+      //     const imgData = canvas.toDataURL('image/png');
+      //     console.log(a4HeightMm)
+      //     let pdf
+      //     // Document of a4WidthMm wide and inputHeightMm high
+      //     if (inputHeightMm > a4HeightMm) {
+      //       // elongated a4 (system print dialog will handle page breaks)
+      //       console.log("in if")
+      //       pdf = new jsPDF('p', 'mm', [inputHeightMm + 99, a4WidthMm]);
+      //     } else {
+      //       console.log("in else")
+      //       // standard a4
+      //       pdf = new jsPDF('p', 'mm', [inputHeightMm + 89, a4WidthMm]);
+      //     }
+
+      //     pdf.addImage(imgData, 'PNG', 0, 0);
+      //     pdf.save(`${id}.pdf`);
+      //   });
       ;
 
       ////////////////////////////////////////////////////////
